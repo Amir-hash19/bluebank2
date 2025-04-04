a7ab4ddcfaa08fb89df2118562f15ef96fd7a099
amirhoseinheydari <amirhosein.hydri1381@gmail.com>
2025-04-04 16:40:58 +0330

    set postgreSQL database

--- bluebank/bluebank/settings.py
+++ bluebank/bluebank/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'bluebank.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bluebank",
+        "USER": "admin",
+        "PASSWORD": "amir112233",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 

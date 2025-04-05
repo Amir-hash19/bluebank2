9e61b4285c8b77efbfee23fe6285485db8c7a8db
amirhoseinheydari <amirhosein.hydri1381@gmail.com>
2025-04-05 18:45:08 +0330

    import postman result and set admin.py

--- bluebank/operation/admin.py
+++ bluebank/operation/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from operation.models import BankAccount, Transaction
 
-# Register your models here.
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ("fullname", "email", "create_at")
+    ordering = ("-create_at", "balance")
+    list_filter = ("create_at", )
+    search_fields = ("full_name", "email", "balance")
+
+
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("account", "amount", "description")
+    ordering = ("-amount", )
+    list_filter = ("amount", "account", "transaction_type")
+    search_fields = ("account", "transaction_type")

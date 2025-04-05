724140494304667e67ab4d98f6388d27c63c32aa
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-04-05 17:32:52 +0330

    set models and migrations

--- bluebank/account/views.py
+++ bluebank/account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+def display_home(request):
+    return HttpResponse("This is home page")
+   
+

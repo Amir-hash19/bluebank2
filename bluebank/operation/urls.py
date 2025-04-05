0d567ac83359405b37975986e8f8e1fd3b488e78
amirhoseinheydari <amirhosein.hydri1381@gmail.com>
2025-04-05 18:51:28 +0330

    END

--- bluebank/operation/urls.py
+++ bluebank/operation/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, transfer_money, withdraw_money
 
 urlpatterns = [
-    path("", home_page, name="home-page")
+    path("", home_page, name="home-page"),
+    path("transfer_money/", transfer_money, name="transfer-money"),
+    path("withdraw_money/", withdraw_money, name="withdraw-money")
 ]
--- bluebank/operation/views.py
+++ bluebank/operation/views.py
@@ -1,7 +1,61 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from .models import Transaction, BankAccount
+from account.models import Account
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 
 
 def home_page(request):
     return HttpResponse("this is oprations page")
+
+
+
+
+@csrf_exempt
+def transfer_money(from_account, to_account, amount):
+    if from_account.balance < amount:
+        raise ValueError("Insufficient funds")
+    
+    from_account.balance -= amount
+    to_account.balance += amount
+
+    from_account.save()
+    to_account.save()
+
+    Transaction.objects.create(
+        account=from_account,
+        transaction_type='transfer',
+        amount=amount,
+        to_account=to_account,
+        description=f"Transaction to{to_account.full_name}"
+    )
+
+
+
+
+
+
+@csrf_exempt
+def withdraw_money(account: Account, amount: float, description: str = ""):
+    if amount <= 0:
+        raise ValueError("Amount must be greater than zero.")
+
+    if account.balance < amount:
+        raise ValueError("Insufficient funds.")
+
+   
+    account.balance -= amount
+    account.save()
+
+  
+    Transaction.objects.create(
+        account=account,
+        transaction_type='withdraw',
+        amount=amount,
+        description=description or "Withdrawal",
+        timestamp=timezone.now()
+    )
+
+    return True
 

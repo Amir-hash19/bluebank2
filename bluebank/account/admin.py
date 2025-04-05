81d53de3129b631718604b8dd11c9bed81b8a9a6
amirhoseinheydari <amirhosein.hydri1381@gmail.com>
2025-04-05 18:08:51 +0330

    set functions

--- bluebank/account/admin.py
+++ bluebank/account/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "email", "create_at")
+    ordering = ("-create_at", )
+    list_filter = ("create_at", )
+    search_fields = ("full_name", "email", "age")
+
+
--- bluebank/account/urls.py
+++ bluebank/account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import display_home
+from .views import display_home, create_account, display_account, remove_account
 
 urlpatterns = [
     path("", display_home, name="display-home"),
+    path("create-account", create_account, name="create-account"),
+    path("display-account/<int:account_id>", display_account, name="display-account"),
+    path("remove-account/<int:account_id>", remove_account, name="remove-account"),
 ]
